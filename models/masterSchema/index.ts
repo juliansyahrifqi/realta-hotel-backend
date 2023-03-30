55afd2784e606771bcc05dea8006f70efc43699c
mhmdkevin07 <mhmdkevin077@gmail.com>
2023-03-30 22:40:27 +0700

    Add Hotel_Kevin

--- models/masterSchema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './regions';
-export * from './country';
-export * from './provinces';
-export * from './address';
-export * from './category_group';
-export * from './city';
-export * from './members';
