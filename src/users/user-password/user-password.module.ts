7976eb0105283120eee5c14c314f6e602375d249
Rifqi Pratama <rifqipratama9@gmail.com>
2023-04-08 05:01:29 +0700

    add forgot password send email

--- src/users/user-password/user-password.module.ts
+++ src/users/user-password/user-password.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserPasswordService } from './user-password.service';
 import { UserPasswordController } from './user-password.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { user_password } from 'models/usersSchema';
+import { user_password, users } from 'models/usersSchema';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([user_password])],
+  imports: [SequelizeModule.forFeature([user_password, users]), MailModule],
   controllers: [UserPasswordController],
   providers: [UserPasswordService],
 })
