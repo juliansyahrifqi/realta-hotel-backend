115885bc78d5a7c2f68b9001a389f01dc766ea8d
Rifqi Pratama <rifqipratama9@gmail.com>
2023-04-07 19:23:24 +0700

    change folder uploads to be a static

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const port = process.env.PORT || 5000;
 
@@ -13,6 +15,8 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
   });
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'uploads'));
 
   await app.listen(port, () => {
     console.log(`App run on port ${port}`);
